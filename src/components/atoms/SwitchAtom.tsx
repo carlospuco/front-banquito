a9172963414e0091aab5f7ffdd56ff06050d4fef
Ismael Sebastián Andrango Peralbo <51093008+SebastianAndrango@users.noreply.github.com>
2023-01-17 14:00:55 -0500

    AF-42: Create Swtich Atom (#6)

---
+++ src/components/atoms/SwitchAtom.tsx
@@ -0,0 +1,29 @@
+import { Stack, Switch, Typography, styled } from '@mui/material';
+import React from 'react';
+
+
+interface Props {
+    name: string;
+    checked: boolean;
+    onChange: () => void;
+    labelOn: string;
+    labelOff: string;
+    color: any;
+}
+
+const SwitchAtom = (props: Props) => {
+    return (
+        <Stack direction="row" spacing={0} alignItems="center">
+            <Typography>{props.labelOff}</Typography>
+            <Switch name={props.name}
+                onChange={props.onChange}
+                defaultChecked={props.checked}
+                color={props.color} />
+            <Typography>{props.labelOn}</Typography>
+        </Stack>
+
+    );
+}
+
+
+export default SwitchAtom;
