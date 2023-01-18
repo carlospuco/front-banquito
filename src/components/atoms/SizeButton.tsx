bb60a7ec3bffc0de961556166dd7db68db1960b1
Francisco David Muñoz Armas <51092954+FranciscoDavidMunozArmas@users.noreply.github.com>
2023-01-18 09:47:02 -0500

    AF-40: Creation of atoms - BigButton, MediumButton, SmallButton (#2)
    * AF-40: Creation of atoms - BigButton, MediumButton, SmallButton
    
    * Imprrove buttons
    
    * AF-40:Remove from app
    
    * AF-40: Improve Button
    
    * Fusion de CUstomButton en SizeButton
    
    * AF-40: Remove styled
    
    * Remove unused imports
    
    Co-authored-by: Damoz1606 <panchitodmun@gmail.com>
    Co-authored-by: Sebastian Alvarez <44406603+listerineh@users.noreply.github.com>

---
+++ src/components/atoms/SizeButton.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Box, Button, Grid } from '@mui/material';
+import { ButtonStyle } from '../../style/ButtonStyle';
+
+interface Props {
+  text: string,
+  style: ButtonStyle,
+  palette: {
+    backgroundColor: string,
+    accent?: string,
+  },
+  onClick: () => void,
+  column?: boolean,
+  icon?: any,
+  size?: {
+    width: number | string,
+    height: number | string
+  }
+}
+
+export const SizeButton = (props: Props) => {
+
+  const configureWidth = () => {
+    let width: number | string;
+    if (props.size) {
+      width = props.size.width;
+    } else {
+      switch (props.style) {
+        case ButtonStyle.BIG:
+          width = '200px';
+          break;
+        case ButtonStyle.MEDIUM:
+          width = '150px';
+          break;
+        case ButtonStyle.SMALL:
+          width = '75px';
+          break;
+      }
+    }
+
+    return width;
+  }
+
+  return (
+    <>
+      <Button
+        variant='contained'
+        disableElevation
+        sx={{
+          backgroundColor: props.palette.backgroundColor,
+          color: props.palette.accent ? props.palette.accent : '#F1FAEE',
+          borderRadius: "10px",
+          transition: "all .3s ease- out",
+          width: configureWidth,
+          ':hover': {
+            backgroundColor: props.palette.accent ? props.palette.accent : 'transparent',
+            color: props.palette.backgroundColor,
+            filter: "opacity(0.75)",
+            transition: "all .3s ease-in-out"
+          }
+        }}
+        onClick={props.onClick}>
+        <Grid
+          display='flex'
+          flexDirection={(props.column) ? 'column' : 'row'}
+          justifyContent='center'
+          alignItems='center'
+          gap={(props.column) ? 0.5 : 1.5}>
+          {props.icon ? <Box display='flex' flexDirection={(props.column != null) ? 'column' : 'row'} justifyContent='center' alignItems='center' sx={{ width: "100%", height: "100%" }}>{props.icon}</Box> : null}
+          <Box display='flex' flexDirection={(props.column != null) ? 'column' : 'row'} justifyContent='center' alignItems='center' sx={{ width: "100%", height: "100%" }}>{props.text}</Box>
+        </Grid>
+      </Button>
+    </>
+  )
+}
---
+++ src/style/ColorPalette.ts
@@ -0,0 +1,7 @@
+export enum ColorPalette{
+    PRIMARY = "#E63946",
+    SECONDARY = "#457B9D",
+    TERNARY = "#1D3557",
+    FOURTH = "#A8DADC",
+    ACCENT = "#F1FAEE"
+}
