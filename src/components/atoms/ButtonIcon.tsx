f2dd5d1412920dcb5dddf9ca8d69762f20524f8b
Francisco David Muñoz Armas <51092954+FranciscoDavidMunozArmas@users.noreply.github.com>
2023-01-18 09:47:57 -0500

    Af 41 crear atomo icon button (#1)
    * AF-41: Initial commit
    
    * Button Icon
    
    * AF-41: Impove icon buttons by adding the floating feature
    
    * AF-41:Solve button icon
    
    * AF-41: Floating function
    
    * Remove styled
    
    * AF-41: Eliminar cambios de App.tsx
    
    * AF-41: Se devuelve unicamente el boton
    
    * AF-41: Eliminacion de Custom CSS
    
    * AF-41: Eliminar used imports
    
    Co-authored-by: Damoz1606 <panchitodmun@gmail.com>
    Co-authored-by: Sebastian Alvarez <44406603+listerineh@users.noreply.github.com>

---
+++ src/components/atoms/ButtonIcon.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Avatar, Button } from '@mui/material';
+
+interface Props {
+    color: string,
+    icon: any,
+    onClick: () => void,
+    float?: boolean,
+    top?: boolean,
+    bottom?: boolean,
+    left?: boolean,
+    right?: boolean,
+    size?: number | string
+}
+
+const ButtonIcon = (props: Props) => {
+
+    return (
+        <Button
+            sx={{
+                borderRadius: '100%',
+                position: props.float ? 'absolute' : 'relative',
+                top: props.top ? 0 : 'auto',
+                bottom: props.bottom ? 0 : 'auto',
+                left: props.left ? 0 : 'auto',
+                right: props.right ? 0 : 'auto',
+                width: props.size ? props.size : '60px',
+                height: props.size ? props.size : '60px',
+                '.MuiTouchRipple-child': {
+                    backgroundColor: props.color
+                }
+            }}>
+            <Avatar sx={{ color: props.color, bgcolor: 'transparent', padding: 0 }}>
+                {props.icon}
+            </Avatar>
+        </Button>
+    )
+}
+
+export default ButtonIcon
