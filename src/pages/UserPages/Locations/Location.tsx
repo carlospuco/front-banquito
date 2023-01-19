3415e48771d46ebc0a4e3f446dd92f12953054a4
Jorge Luis Galarza Cruz <44406615+jlgalarza3@users.noreply.github.com>
2023-01-19 11:07:24 -0500

    Selección de acción para la gestión de ubicaciones mediante Tabs de MUI (#15)
    * Fix Router language and Organizate user folders
    
    * Add Location Tabs
    
    * Adress Sebas Comment
    
    Co-authored-by: Sebastian Alvarez <44406603+listerineh@users.noreply.github.com>

--- src/pages/UserPages/Locations/Location.tsx
+++ src/pages/UserPages/Locations/Location.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { LocationTabs } from "../../../components/organisms/Location/LocationTabs";
+
+const boxStyles = () => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  height: "95vh",
+});
 
 export const Location = () => {
-  return <div>Location</div>;
+  const [tabValue, setTabValue] = useState("Ver");
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    setTabValue(newValue);
+  };
+
+  return (
+    <Box sx={boxStyles}>
+      <LocationTabs tabValue={tabValue} handleChange={handleChange} />
+      <Box sx={{ width: "80%" }}>
+        {tabValue === "Ver" && <div>Ver Componente</div>}
+        {tabValue === "Crear" && <div>Crear Componente</div>}
+        {tabValue === "Actualizar" && <div>Actualizar Componente</div>}
+        {tabValue === "Eliminar" && <div>Eliminar Componente</div>}
+      </Box>
+    </Box>
+  );
 };
