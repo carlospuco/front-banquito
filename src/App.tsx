270ab467259de35efd84efb9f899b8dfe59b54ec
kevincaicedo1 <kocaicedo@espe.edu.ec>
2023-01-21 20:38:25 -0500

    FP-5 Cambios interfaces interestRate

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
     //     <Route path="*" element={<Error404 />} />
     //   </Routes>
     // </BrowserRouter
-    <FormInterestRateLog />
+    <InterestRateLog />
   )
 }
 
