3900b46cd3f9bb0e82e360ec4e73b5741c0b846a
Jorge Luis Galarza Cruz <44406615+jlgalarza3@users.noreply.github.com>
2023-01-23 00:00:48 -0500

    Add view location table (#42)

--- src/components/organisms/Location/ViewLocations.tsx
+++ src/components/organisms/Location/ViewLocations.tsx
@@ -1,41 +1,79 @@
-import { Typography } from '@mui/material';
-import TableMolecule from '../../molecules/TableMolecule';
-import * as React from 'react';
+import React, { useEffect, useState } from "react";
+import { Container, Typography, TextField } from "@mui/material";
+import TableMolecule from "../../molecules/TableMolecule";
+import { Province } from "./types";
+import { getProvinces } from "./functions";
+
 const headers = [
-    <Typography>
-        País
-    </Typography>,
-    <Typography>
-        Provincia
-    </Typography>,
-    <Typography>
-        Cantón
-    </Typography>,
-    <Typography>
-        Parroquia
-    </Typography>,
+  <Typography>Provincia</Typography>,
+  <Typography>Cantón</Typography>,
+  <Typography>Parroquia</Typography>,
+  <Typography>Código Postal</Typography>,
 ];
 
-const rows = [[
-    <Typography>
-        1
-    </Typography>,
-    <Typography>
-        Tasa de interes 1
-    </Typography>,
-    <Typography>
-        Descripción de la tasa de interes 1
-    </Typography>,
-    <Typography>
-        s
-    </Typography>,
-],
-];
 export const ViewLocations = () => {
-    return (
-        <div>
-        <h1>Ver Ubicaciones</h1>
-        <TableMolecule headers={headers} rows={rows} />
-        </div>
-    );
-    };
+  const [provinces, setProvinces] = useState<Province[]>([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    getProvinces().then(setProvinces);
+  }, []);
+
+  const filteredRows =
+    provinces.length > 0
+      ? provinces
+          .flatMap((province) =>
+            province.cantons.flatMap((canton) =>
+              canton.parishes
+                .filter((parish) => {
+                  return (
+                    parish.parishName.toLowerCase().indexOf(searchText) > -1 ||
+                    canton.cantonName.toLowerCase().indexOf(searchText) > -1 ||
+                    province.provinceName.toLowerCase().indexOf(searchText) > -1
+                  );
+                })
+                .map((parish) => [
+                  <Typography>{province.provinceName}</Typography>,
+                  <Typography>{canton.cantonName}</Typography>,
+                  <Typography>{parish.parishName}</Typography>,
+                  <Typography>{parish.zipCode}</Typography>,
+                ])
+            )
+          )
+          .filter((row) => row.length > 0)
+      : [];
+
+  return (
+    <Container style={containerStyle}>
+      <Container style={{ textAlign: "center" }}>
+        <Typography variant="h4" align="center">
+          Ver Ubicaciones
+        </Typography>
+        <Container sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <TextField
+            label="Buscar por provincia, cantón o parroquia"
+            value={searchText}
+            onChange={(event) =>
+              setSearchText(event.target.value.toLowerCase())
+            }
+            sx={{ width: "350px" }}
+          />
+        </Container>
+      </Container>
+      <Container style={containerTableStyle}>
+        <TableMolecule headers={headers} rows={filteredRows} />
+      </Container>
+    </Container>
+  );
+};
+
+const containerTableStyle = {
+  marginTop: "10px",
+};
+
+const containerStyle = {
+  display: "flex",
+  "flex-direction": "column",
+  "align-items": "flex-start",
+  marginTop: "70px",
+};
